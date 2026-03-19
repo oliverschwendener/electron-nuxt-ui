a65d1f6288b45806ad7754c7027396ef92bfad32
Oliver Schwendener <oliver.schwendener@proton.me>
2026-03-19 13:45:40 +0100

    Stuff

--- src/renderer/Home.vue
+++ src/renderer/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <UContainer> Home </UContainer>
+    <UContainer>
+        <div class="flex justify-center items-center h-screen flex-col gap-4">
+            <h1 class="text-4xl font-bold">Welcome to Electron + Nuxt UI</h1>
+            <UButton @click="increment">Click me! {{ count }}</UButton>
+            <UColorModeSelect />
+        </div>
+    </UContainer>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+    count.value++;
+};
+</script>
